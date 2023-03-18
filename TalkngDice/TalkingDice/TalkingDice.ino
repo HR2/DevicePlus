b43feb6d2118866488159dadb07add7458562663
DESKTOP-4KS6218\hirah <hirahara.makoto@gmail.com>
2023-03-18 18:17:50 +0900

    TalkingDice.ino

--- TalkngDice/TalkingDice/TalkingDice.ino
+++ TalkngDice/TalkingDice/TalkingDice.ino
@@ -15,28 +15,25 @@ KXTJ3 kxtj3(KXTJ3_ADDR_LOW);  //加速度センサモジュールのインスタ
 #define ERROR_WAIT 1000           // エラー表示の待機時間（ms）
 void error_func(int32_t result);  //加速度センサモジュールのエラー表示関数
 
-const int volume = 5;  // ボリューム。最大30
-const int maxDiffIndex = 10;
-const int buttonPin = 7;
+const int volume = 20;          // ボリューム。最大30
+const int bufferIndexMax = 5;  //平均化するバッファサイズ
+const int buttonPin = 7;       //ボタンに接続するピン
+const int modeNum = 3;         //モードの数
 
-int diffIndex = 0;
-float32 accPre[3] = { 0.0, 0.0, 0.0 };  //1つ前の加速度[x,y,z]
-float32 accDiffArr[3][maxDiffIndex];    //差分の履歴[x,y,z][maxDiffIndex]
+int bufferIndex = 0;                   //バッファのインデックス
+float32 accBuffer[3][bufferIndexMax];  //加速度の履歴
 
 float32 tolerance = 0.5;       //出目判定時の許容範囲
 float32 stopTolerance = 0.03;  //静止判定時の許容範囲　小さいほど、判定がシビア
-float32 shakeTolerance = 0.1;  //揺れ判定時の許容範囲　大きいほど、大きく振る必要がある
+float32 shakeTolerance = 0.3;  //揺れ判定時の許容範囲　大きいほど、大きく振る必要がある
 
 bool standbyFlag = true;  //サイコロを振る準備ができているか
 int mode = 0;             //選択中のゲームモード。
-int modeNum = 3;          //モードの数。
 
-//----------------------------------------------
 
 void setup() {
   pinMode(buttonPin, INPUT_PULLUP);  //ボタンに接続したピンをプルアップの入力として設定する
   Serial.begin(SYSTEM_BAUDRATE);     //シリアル通信を開始
-
 
   //-------------------------------------------
   //DFPlayer miniのセットアップ
@@ -51,9 +48,6 @@ void setup() {
     }
   }
   Serial.println(F("DFPlayer Mini online."));
-
-  myDFPlayer.volume(volume);  //音量（ 0～30 ）
-  myDFPlayer.play(7);     //ゲームモード1からスタート
 
   //-------------------------------------------
   //加速度センサモジュールのセットアップ
@@ -72,6 +66,11 @@ void setup() {
     error_func(result);  //通信に失敗していたらエラーメッセージを表示
   }
 
+  //-------------------------------------------
+  myDFPlayer.volume(volume);  //音量（ 0～30 ）
+  myDFPlayer.play(7);         //モード1のタイトル
+  delay(3000);
+
   return;
 }
 
@@ -81,15 +80,14 @@ void setup() {
 
 
 void loop() {
-
   //モードの切替え -------------------------------------
   if (digitalRead(buttonPin) == LOW) {  //ボタンが押された場合
     mode++;
-    if (mode > modeNum) mode = 0;
+    if (mode >= modeNum) mode = 0;
     Serial.print("MODE : ");
     Serial.println(mode);
     myDFPlayer.play((mode + 1) * 7);  //タイトル音声を再生
-    delay(1000);
+    delay(3000);
   }
 
   // 姿勢の検出 -----------------------------------------
@@ -97,89 +95,71 @@ void loop() {
   float32 acc[KXTJ3_AXIS_SIZE];
   result = kxtj3.get_val(acc);  //加速度センサの値を取得
 
+  float32 accAve[3] = { 0.0, 0.0, 0.0 };   //バッファの平均
+  float32 accDiff[3] = { 0.0, 0.0, 0.0 };  //平均と現在の差
+
+
   if (result == KXTJ3_COMM_OK) {  //加速度センサとの通信に成功していたら
 
-    //前と現在の加速度を比較
-    float32 accDiff[KXTJ3_AXIS_SIZE];
-    accDiff[0] = accPre[0] - acc[0];
-    accDiff[1] = accPre[1] - acc[1];
-    accDiff[2] = accPre[2] - acc[2];
-
-    //前の加速度を更新する
-    accPre[0] = acc[0];
-    accPre[1] = acc[1];
-    accPre[2] = acc[2];
-
     //インデックスを増やし、上限を超えたら0にする
-    diffIndex++;
-    if (diffIndex > maxDiffIndex) diffIndex = 0;
+    bufferIndex++;
+    if (bufferIndex >= bufferIndexMax) bufferIndex = 0;
 
-    //差を配列に保存する
-    accDiffArr[0][diffIndex] = accDiff[0];
-    accDiffArr[1][diffIndex] = accDiff[1];
-    accDiffArr[2][diffIndex] = accDiff[2];
+    //加速度をバッファに入れる
+    accBuffer[0][bufferIndex] = acc[0];
+    accBuffer[1][bufferIndex] = acc[1];
+    accBuffer[2][bufferIndex] = acc[2];
 
-    //差を平均する
-    float32 accDiffAve[3] = { 0.0, 0.0, 0.0 };
-    for (int i = 0; i < maxDiffIndex; i++) {
-      accDiffAve[0] += accDiffArr[0][i] * 1.0 / maxDiffIndex;
-      accDiffAve[1] += accDiffArr[1][i] * 1.0 / maxDiffIndex;
-      accDiffAve[2] += accDiffArr[2][i] * 1.0 / maxDiffIndex;
+    //平均する
+    for (int i = 0; i < bufferIndexMax; i++) {
+      accAve[0] += accBuffer[0][i] / bufferIndexMax;
+      accAve[1] += accBuffer[1][i] / bufferIndexMax;
+      accAve[2] += accBuffer[2][i] / bufferIndexMax;
     }
 
-    Serial.print("x:");
-    Serial.print(accDiffAve[0]);
-    Serial.print("  y:");
-    Serial.print(accDiffAve[1]);
-    Serial.print("  z:");
-    Serial.println(accDiffAve[2]);
+    //平均と現在の差
+    accDiff[0] = abs(accAve[0] - acc[0]);
+    accDiff[1] = abs(accAve[1] - acc[1]);
+    accDiff[2] = abs(accAve[2] - acc[2]);
 
-    //差の平均が小さくなる、つまり揺れが収まったら
-    if (-stopTolerance < accDiffAve[0] && accDiffAve[0] < stopTolerance && -stopTolerance < accDiffAve[1] && accDiffAve[1] < stopTolerance && -stopTolerance < accDiffAve[2] && accDiffAve[2] < stopTolerance) {
+    //平均との差が小さくなる、つまり揺れが収まったら
+    if (accDiff[0] < stopTolerance && accDiff[1] < stopTolerance && accDiff[2] < stopTolerance) {
       Serial.println("静止");
+
       if (standbyFlag == true) {
         Serial.println("再生");
-
         standbyFlag = false;
 
-        //上(1)の音声を再生
+        //出目に対応した音声を再生
         if ((-tolerance < acc[0] && acc[0] < tolerance) && (-tolerance < acc[1] && acc[1] < tolerance) && (1 - tolerance < acc[2] && acc[2] < 1 + tolerance)) {
+          Serial.println("出目1");
           myDFPlayer.play(1 + mode * 7);
-        }
-
-        //前(2)の音声を再生
-        if ((-1 - tolerance < acc[0] && acc[0] < -1 + tolerance) && (-tolerance < acc[1] && acc[1] < tolerance) && (-tolerance < acc[2] && acc[2] < tolerance)) {
+        }else if ((-1 - tolerance < acc[0] && acc[0] < -1 + tolerance) && (-tolerance < acc[1] && acc[1] < tolerance) && (-tolerance < acc[2] && acc[2] < tolerance)) {
+          Serial.println("出目2");
           myDFPlayer.play(2 + mode * 7);
-        }
-
-        //左(3)の音声を再生
-        if ((-tolerance < acc[0] && acc[0] < tolerance) && (1 - tolerance < acc[1] && acc[1] < 1 + tolerance) && (-tolerance < acc[2] && acc[2] < tolerance)) {
+        }else if ((-tolerance < acc[0] && acc[0] < tolerance) && (1 - tolerance < acc[1] && acc[1] < 1 + tolerance) && (-tolerance < acc[2] && acc[2] < tolerance)) {
+          Serial.println("出目3");
           myDFPlayer.play(3 + mode * 7);
-        }
-
-        //右(4)の音声を再生
-        if ((-tolerance < acc[0] && acc[0] < tolerance) && (-1 - tolerance < acc[1] && acc[1] < -1 + tolerance) && (-tolerance < acc[2] && acc[2] < tolerance)) {
+        }else if ((-tolerance < acc[0] && acc[0] < tolerance) && (-1 - tolerance < acc[1] && acc[1] < -1 + tolerance) && (-tolerance < acc[2] && acc[2] < tolerance)) {
+          Serial.println("出目4");
           myDFPlayer.play(4 + mode * 7);
-        }
-
-        //後(5)の音声を再生
-        if ((1 - tolerance < acc[0] && acc[0] < 1 + tolerance) && (-tolerance < acc[1] && acc[1] < tolerance) && (-tolerance < acc[2] && acc[2] < tolerance)) {
+        }else if ((1 - tolerance < acc[0] && acc[0] < 1 + tolerance) && (-tolerance < acc[1] && acc[1] < tolerance) && (-tolerance < acc[2] && acc[2] < tolerance)) {
+          Serial.println("出目5");
           myDFPlayer.play(5 + mode * 7);
-        }
-
-        //下(6)の音声を再生
-        if ((-tolerance < acc[0] && acc[0] < tolerance) && (-tolerance < acc[1] && acc[1] < tolerance) && (-1 - tolerance < acc[2] && acc[2] < -1 + tolerance)) {
+        }else if ((-tolerance < acc[0] && acc[0] < tolerance) && (-tolerance < acc[1] && acc[1] < tolerance) && (-1 - tolerance < acc[2] && acc[2] < -1 + tolerance)) {
+          Serial.println("出目6");
           myDFPlayer.play(6 + mode * 7);
         }
+
+        delay(3000);
       }
     }
 
     //大きく揺れた場合
-    if (accDiffAve[0] < -shakeTolerance || shakeTolerance < accDiffAve[0] || accDiffAve[1] < -shakeTolerance || shakeTolerance < accDiffAve[1] || accDiffAve[2] < -shakeTolerance || shakeTolerance < accDiffAve[2]) {
-      Serial.println("ゆれ判定");
+    if (shakeTolerance < accDiff[0] || shakeTolerance < accDiff[1] || shakeTolerance < accDiff[2]) {
+      Serial.println("揺れ");
       standbyFlag = true;  //準備状態に戻す
     }
-
 
   } else {
     //加速度センサとの通信に失敗していたら
@@ -189,6 +169,7 @@ void loop() {
 
   delay(SYSTEM_WAIT);
 }
+
 
 //加速度センサのエラーメッセージ
 void error_func(int32_t result) {
